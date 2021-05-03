cb30c5e0f69326c52e17bf68e99609de27f5a10b
Deepak9242 <deepakkumarsharma9242@gmail.com>
2021-05-03 13:22:18 -0800

    number_nullet

--- src/App.js
+++ src/App.js
@@ -2,58 +2,12 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  const nm = [1, 2, 3, 4, 5, 6, 7, 8];
+  const dm = nm.map(x => <li>{x * 2}</li>);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{nm}</h1>
+      <ul>{dm}</ul>
     </div>
   );
-}
-
-export function MailBox(props) {
-  const msg = props.number;
-  let body;
-  if (msg > 0) {
-    body = <h2> You have {msg} Unread message </h2>;
-  } else {
-    body = <h2> You have No Unread messages </h2>;
-  }
-
-  return body;
-}
-
-export class MailControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleBack = this.handleBack.bind(this);
-    this.state = {
-      num: 0
-    };
-  }
-
-  handleClick() {
-    this.setState(state => ({
-      num: state.num + 1
-    }));
-  }
-
-  handleBack() {
-    if (this.state.num > 0) {
-      this.setState(state => ({
-        num: state.num - 1
-      }));
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}> Add </button>
-
-        <button onClick={this.handleBack}> Dec </button>
-        <MailBox number={this.state.num} />
-      </div>
-    );
-  }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { MailControl } from "./App";
 
-//ReactDOM.render(<App />, document.getElementById("footer"));
-ReactDOM.render(<MailControl />, document.getElementById("third"));
+ReactDOM.render(<App />, document.getElementById("footer"));
+//ReactDOM.render(<MailControl />, document.getElementById("third"));
